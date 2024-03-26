25f35b558496b23950cfede9305573047c88f241
Costin <costin.vieru@gmail.com>
2024-03-26 10:03:24 +0200

    Initial Commit

---
+++ BBOTHEAPP/BBOTHEAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBOTHEAPPApp.swift
+//  BBOTHEAPP
+//
+//  Created by Costin Vieru on 26.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct BBOTHEAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
