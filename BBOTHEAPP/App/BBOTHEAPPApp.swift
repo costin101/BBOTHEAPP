a1aa565e56fb5d1154475970d0942052fc6e8894
Costin <costin.vieru@gmail.com>
2024-03-27 21:19:58 +0200

    deprecated webapp proper

---
+++ BBOTHEAPP/App/BBOTHEAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBOTHEAPPApp.swift
+//  BBOTHEAPP
+//
+//  Created by Costin Vieru on 04.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BBOTHEAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
